abd0c95643fc42281ca229ce4802e4345c6f0675
wojtasss <wojcieh.rzepecki@gmail.com>
2015-10-27 22:42:40 +0100

    Update Matrix.java

--- projekt_1/Matrix.java
+++ projekt_1/Matrix.java
@@ -2,15 +2,60 @@ public class Matrix
 {
     private int degree;
     private int matrix[][];
+    private int rows;
+    private int cols;
+    private boolean isSquare;
     
     public Matrix(int values[], int degree)
     {
         this.degree = degree;
+        this.rows = degree;
+        this.cols = degree;
+        this.isSquare = true;
         this.matrix = fillMatrix(values, degree);
     }
+    public Matrix(int values[], int rows, int cols)
+    {
+    	this.rows = rows;
+    	this.cols = cols;
+    	this.isSquare = false;
+    	this.matrix = fillMatrix(values, rows, cols);
+    }
+    public Matrix(Matrix matrix)
+    {
+    	this.degree = matrix.getDegree();
+    	this.matrix = copyMatrix(matrix.getMatrix(), matrix.getRows(), matrix.getCols());
+    	this.rows = matrix.getRows();
+    	this.cols = matrix.getCols();
+    	this.isSquare = matrix.isSquareMatrix();
+    }
     
-    public int getDegree(){ return this.degree; }
+    public boolean isSquareMatrix() { return this.isSquare; }
+    public int getDegree() { return this.degree; }
     public int getValue(int i, int j) { return this.matrix[i][j]; }
+    public int[][] getMatrix() { return matrix;}
+    public int getRows() { return this.rows; }
+    public int getCols() { return this.cols; }
+    
+    private int [][] copyMatrix(int matrix[][], int rows, int cols)
+    {
+    	int newMatrix[][] = new int[rows][cols];
+    	
+    	for(int i=0; i<rows; i++)
+    	{
+    		for(int j=0; j<cols; j++)
+    		{
+    			newMatrix[i][j] = matrix[i][j];
+    		}
+    	}
+    	
+    	return newMatrix;
+    }
+    
+    public void setValue(int i, int j, int value)
+    {
+    	this.matrix[i][j] = value;
+    }
     
     private int[][] fillMatrix(int values[], int deegre)
     {
@@ -27,47 +72,64 @@ public class Matrix
         return newMatrix;
     }
     
+    private int[][] fillMatrix(int values[], int rows, int cols)
+    {
+    	int newMatrix[][] = new int[rows][cols];
+    	
+    	for(int i=0; i<rows; i++)
+    	{
+    		for(int j=0; j<cols; j++)
+    		{
+    			newMatrix[i][j] = values[i*rows+j];
+    		}
+    	}
+    	
+    	return newMatrix;
+    }
+    
     public void displayMatrix()
     {
-        for(int i=0; i<this.degree; i++)
+        for(int i=0; i<this.rows; i++)
         {
-            for(int j=0; j<this.degree; j++)
+            for(int j=0; j<this.cols; j++)
             {
                 System.out.print(this.matrix[i][j] + " ");
             }
             
             System.out.println();
         }
+        
+        System.out.println();
     }
     
-    public static Matrix createMatrix(int values[], int degree)
-    {
-        if((int)Math.pow((double)degree, 2.0) == values.length)
-        {
-            return new Matrix(values, degree);
-        }
-        else
-        {
-            return null;
-        }
+    public static boolean isSquareMatrix(Matrix matrix) 
+    { 
+    	return matrix.isSquareMatrix(); 
     }
     
     public static int computeIndicator(Matrix matrix)
     {
-        if(matrix.getDegree() == 1)
-        {
-            return matrix.getValue(0, 0);
-        }
-        
-        int result=0;
-        
-        for(int i=0; i<matrix.getDegree(); i++)
-        {
-            int arrayValues[] = getMatrixValues(matrix, i);
-            result += matrix.getValue(i, 0)*(int)Math.pow((double)-1, i)*Matrix.computeIndicator(new Matrix(arrayValues, matrix.getDegree()-1));   
-        }
-        
-        return result;
+    	if(matrix.isSquareMatrix())
+    	{
+	        if(matrix.getDegree() == 1)
+	        {
+	            return matrix.getValue(0, 0);
+	        }
+	        
+	        int result=0;
+	        
+	        for(int i=0; i<matrix.getDegree(); i++)
+	        {
+	            int arrayValues[] = getMatrixValues(matrix, i);
+	            result += matrix.getValue(i, 0)*(int)Math.pow((double)-1, i)*Matrix.computeIndicator(new Matrix(arrayValues, matrix.getDegree()-1));   
+	        }
+	        
+	        return result;
+    	}
+    	else
+    	{
+    		return -1;
+    	}
     }
     
     private static int[] getMatrixValues(Matrix matrix, int elem)
@@ -89,5 +151,33 @@ public class Matrix
         }
         
         return arrayValues;
+    }
+    
+    public static Matrix changedMatrix(Matrix matrix, int yValues[], int col)
+    {	  
+	   	 for(int i=0; i<yValues.length; i++)
+	   	 {
+	   		 matrix.setValue(i, col, yValues[i]);
+	   	 }
+	   	 
+	   	 //matrix.displayMatrix();
+	   	 
+	   	 return matrix;
+    }
+    
+    public static int[] fillMatrixValues(int xValues[])
+    {
+	   	 int numberOfMatrixFields = (int)Math.pow((double) xValues.length, 2.0);
+	   	 int matrixValues[] = new int[numberOfMatrixFields];
+	   	 
+	   	 for(int i=0; i<xValues.length; i++)
+	   	 {
+	   		 for(int j=0; j<xValues.length; j++)
+	   		 {
+	   			 matrixValues[i*xValues.length+j] = (int)Math.pow((double)xValues[i], (double)j);
+	   		 }
+	   	 }
+   	 
+   	 	return matrixValues;
     }
 }
