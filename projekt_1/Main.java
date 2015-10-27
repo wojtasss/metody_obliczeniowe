70945a93a41237e1fb57fda5771db5fa3f0bbf34
wojtasss <wojcieh.rzepecki@gmail.com>
2015-10-27 15:07:33 +0100

    Update Main.java

--- projekt_1/Main.java
+++ projekt_1/Main.java
@@ -3,7 +3,8 @@ public class Main
      public static void main(String []args)
      {
         int values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        Matrix matrix = Matrix.createMatrix(values, (int)Math.sqrt((double)values.length));
+        int matrixDegree = (int)Math.sqrt((double)values.length);
+        Matrix matrix = Matrix.createMatrix(values, matrixDegree);
         
         if(!(matrix == null))
         {
